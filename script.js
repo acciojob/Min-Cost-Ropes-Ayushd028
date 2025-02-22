c9c1daf2873ce42ef67dd96afb4c6a62a52bbde4
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-02-22 20:59:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+	let cost = 0;
+	arr.sort((a, b) => a - b);
+
+	while(arr.length>1){
+		
+		let first = arr.shift();
+		let second = arr.shift();
+	
+		let newRope = first + second;
+	
+		cost+=newRope;
+		
+		arr.push(newRope);
+	    arr.sort((a, b) => a - b);
+	}
+	return cost;
 }
 
 module.exports=mincost;
